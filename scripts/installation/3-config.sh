5f303286567e93c7cf6cc6e45f1ebd22f6c9c54d
nigelwarning <nigelwestland@gmail.com>
2021-01-16 12:12:17 +0100

    icons

--- scripts/installation/3-config.sh
+++ scripts/installation/3-config.sh
@@ -33,6 +33,7 @@ ln -s ~/archdots/.config/Kvantum ~/.config/Kvantum
 ln -s ~/archdots/.config/qt5ct ~/.config/qt5ct
 ln -s ~/archdots/.config/mimeapps.list ~/.config/mimeapps.list
 ln -s ~/archdots/.local/kservices5 ~/.local/share/kserivces5
+ln -s ~/archdots/Papirus-Dark-Mint-Y-Materia ~/.icons/Papirus-Dark-Mint-Y-Materia
 
 
 sudo systemctl enable tlp
