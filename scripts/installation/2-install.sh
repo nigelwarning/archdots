8a5eae0b28d63b4694204d0990f44c9c48c47851
nigelwarning <nigelwestland@gmail.com>
2021-01-16 12:30:45 +0100

    mint-y-icons

--- scripts/installation/2-install.sh
+++ scripts/installation/2-install.sh
@@ -1,3 +1,3 @@
-yay -S tuxedo-keyboard tuxedo-control-center shadow-tech opensubtitles-uploader
+yay -S tuxedo-keyboard tuxedo-control-center shadow-tech opensubtitles-uploader mint-y-icons
 
 yay bauh
