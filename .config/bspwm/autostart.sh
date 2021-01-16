20dba63ad6f0dc60ae67062a1bdf6834020c712c
nigelwarning <nigelwestland@gmail.com>
2021-01-16 13:22:38 +0100

    scripts

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -16,10 +16,8 @@ xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 nitrogen --restore &
-run nm-applet &
 run xfce4-power-manager &
 numlockx on &
-blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
