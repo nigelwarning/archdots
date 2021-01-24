9ccbef20c27fcd3e0265cd1073c88cd2594e4461
nigelwarning <nigelwestland@gmail.com>
2021-01-24 14:20:42 +0100

    new update

--- scripts/installation/1-repos.sh
+++ scripts/installation/1-repos.sh
@@ -1,5 +1,4 @@
-yay -S arch4edu-keyring 
-yay chaotic-keyring
+yay -S arch4edu-keyring chaotic-keyring
 
 sudo pacman-key --keyserver hkp://pool.sks-keyservers.net -r 003DB8B0CB23504F
 sudo pacman-key --lsign 003DB8B0CB23504F
--- scripts/installation/2-install.sh
+++ scripts/installation/2-install.sh
@@ -1,4 +1,4 @@
-yay -S linux-lts linux-lts-headers libva-intel-driver tlp intel-undervolt noto-fonts-emoji docker tela-icon-theme-git gwenview appimagelauncher htop discord reflector kio-gdrive ffmpegthumbs qogir-kde-theme-git qogir-gtk-theme-git eos-update-notifier grub-customizer
+yay -S linux-lts linux-lts-headers libva-intel-driver tlp intel-undervolt noto-fonts-emoji docker tela-icon-theme-git gwenview appimagelauncher htop discord reflector kio-gdrive ffmpegthumbs qogir-kde-theme-git qogir-gtk-theme-git eos-update-notifier grub-customizer kvantum-qt5 scrot
 
 yay -S tuxedo-keyboard tuxedo-control-center shadow-tech opensubtitles-uploader snapd snapd-glib flatpak plasma5-applets-virtual-desktop-bar-git
 
