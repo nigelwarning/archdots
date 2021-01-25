d2d379ed5f6fb6b5d7da1370e7fe11eba7654e33
nigelwarning <nigelwestland@gmail.com>
2021-01-25 21:38:23 +0100

    bspwm

--- scripts/installation/1-repos.sh
+++ scripts/installation/1-repos.sh
@@ -1,4 +1,5 @@
-yay -S arch4edu-keyring chaotic-keyring
+yay -S arch4edu-keyring 
+yay chaotic-keyring
 
 sudo pacman-key --keyserver hkp://pool.sks-keyservers.net -r 003DB8B0CB23504F
 sudo pacman-key --lsign 003DB8B0CB23504F
