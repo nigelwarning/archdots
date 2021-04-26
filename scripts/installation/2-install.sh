da735225e956a48217a1c58756f5d44eff093807
nigelwarning <nigelwestland@gmail.com>
2021-04-26 18:08:22 +0200

    new breeze

--- scripts/installation/2-install.sh
+++ scripts/installation/2-install.sh
@@ -12,14 +12,11 @@ yay -S gwenview kio-gdrive ffmpegthumbs scrot
 
 #Utilities
 
-yay -S noto-fonts-emoji appimagelauncher htop reflector eos-update-notifier kvantum-qt5
+yay -S noto-fonts-emoji appimagelauncher htop reflector eos-update-notifier
 
-#Themes
-
-yay -S tela-icon-theme-git
 #AUR
 
-yay -S tuxedo-keyboard tuxedo-control-center opensubtitles-uploader snapd snapd-glib flatpak plasma5-applets-virtual-desktop-bar-git google-chrome
+yay -S tuxedo-keyboard tuxedo-control-center opensubtitles-uploader snapd snapd-glib flatpak google-chrome
 
 #Virtualbox
 yay -S --needed --noconfirm virtualbox-host-modules-arch virtualbox xf86-video-vmware
