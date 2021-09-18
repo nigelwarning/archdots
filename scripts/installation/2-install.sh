a55c86c9fa6386c0c482c042835dd2d3c9e4f227
nigelwarning <nigelwestland@gmail.com>
2021-09-19 00:11:12 +0200

    gnome

--- scripts/installation/2-install.sh
+++ scripts/installation/2-install.sh
@@ -8,15 +8,15 @@ yay -S docker discord grub-customizer
 
 #KDE Apps
 
-yay -S gwenview kio-gdrive ffmpegthumbs
+yay -S ffmpegthumbnailer gst-libav gst-plugins-ugly
 
 #Utilities
 
-yay -S noto-fonts-emoji appimagelauncher htop reflector layan-kde-git layan-gtk-theme-git kvantum-theme-layan-git tela-circle-icon-theme-git flameshot
+yay -S noto-fonts-emoji appimagelauncher htop reflector layan-gtk-theme-git kvantum-theme-layan-git tela-circle-icon-theme-git flameshot
 
 #AUR
 
-yay -S tuxedo-keyboard tuxedo-control-center opensubtitles-uploader snapd snapd-glib flatpak google-chrome konsave pulseaudio-modules-bt
+yay -S tuxedo-keyboard tuxedo-control-center opensubtitles-uploader snapd snapd-glib flatpak google-chrome konsave pulseaudio-modules-bt gnome-shell-extension-dash-to-dock gnome-shell-extension-appindicator
 
 #Virtualbox
 yay -S --needed --noconfirm virtualbox-host-modules-arch virtualbox xf86-video-vmware
