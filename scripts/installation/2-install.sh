37b279610021bec6327857547fb5a7705cc3f286
nigelwarning <nigelwestland@gmail.com>
2021-09-21 14:11:15 +0200

    bspwm back

--- scripts/installation/2-install.sh
+++ scripts/installation/2-install.sh
@@ -1,22 +1,19 @@
 #Basic
 
-yay -S linux-lts linux-lts-headers intel-media-driver tlp
+yay -S linux-lts linux-lts-headers intel-media-driver geany dmenu ranger
 
 #Applications
 
-yay -S docker discord grub-customizer
+yay -S docker discord grub-customizer xcursor-breeze
 
-#KDE Apps
-
-yay -S ffmpegthumbnailer gst-libav gst-plugins-ugly
 
 #Utilities
 
-yay -S noto-fonts-emoji appimagelauncher htop reflector layan-gtk-theme-git kvantum-theme-layan-git tela-circle-icon-theme-git flameshot
+yay -S noto-fonts-emoji appimagelauncher flameshot papirus-icon-theme materia-gtk-theme kvantum-theme-materia
 
 #AUR
 
-yay -S tuxedo-keyboard tuxedo-control-center opensubtitles-uploader snapd snapd-glib flatpak google-chrome konsave pulseaudio-modules-bt gnome-shell-extension-dash-to-dock gnome-shell-extension-appindicator
+yay -S tuxedo-keyboard tuxedo-control-center opensubtitles-uploader snapd snapd-glib flatpak google-chrome pulseaudio-modules-bt qt5ct kvantum-qt5
 
 #Virtualbox
 yay -S --needed --noconfirm virtualbox-host-modules-arch virtualbox xf86-video-vmware
