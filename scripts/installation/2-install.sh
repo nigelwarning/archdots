91902b7694e8a0fac447784a5f0a09bfcfd2290d
nigelwarning <nigelwestland@gmail.com>
2021-09-22 00:05:46 +0200

    gnome

--- scripts/installation/2-install.sh
+++ scripts/installation/2-install.sh
@@ -1,19 +1,22 @@
 #Basic
 
-yay -S linux-lts linux-lts-headers intel-media-driver geany dmenu kitty
+yay -S linux-lts linux-lts-headers intel-media-driver tlp
 
 #Applications
 
-yay -S docker discord grub-customizer xcursor-breeze
+yay -S docker discord grub-customizer
 
+#KDE Apps
+
+yay -S ffmpegthumbnailer gst-libav gst-plugins-ugly
 
 #Utilities
 
-yay -S noto-fonts-emoji appimagelauncher flameshot papirus-icon-theme materia-gtk-theme kvantum-theme-materia bluez bluez-utils dconf-editor
+yay -S noto-fonts-emoji appimagelauncher htop reflector layan-gtk-theme-git kvantum-theme-layan-git tela-circle-icon-theme-git flameshot
 
 #AUR
 
-yay -S tuxedo-keyboard tuxedo-control-center opensubtitles-uploader snapd snapd-glib flatpak google-chrome pulseaudio-modules-bt qt5ct kvantum-qt5
+yay -S tuxedo-keyboard tuxedo-control-center opensubtitles-uploader snapd snapd-glib flatpak google-chrome konsave pulseaudio-modules-bt gnome-shell-extension-dash-to-dock gnome-shell-extension-appindicator
 
 #Virtualbox
 yay -S --needed --noconfirm virtualbox-host-modules-arch virtualbox xf86-video-vmware
--- scripts/installation/3-config.sh
+++ scripts/installation/3-config.sh
@@ -1,3 +1,5 @@
+sudo groupadd nigel
+sudo gpasswd -a nigel nigel
 sudo usermod -aG docker nigel
 
 cd /opt
@@ -15,39 +17,23 @@ sudo chown -R nigel:nigel nigel
 
 cd
 
-sudo rm -rf .bashrc .icons 
-
-cd .config
-sudo rm -rf mimeapps.list bspwm sxhkd dunst polybar rofi termite nitrogen gtk-3.0 xfce4
+sudo rm -rf .bashrc 
 
 ln -s ~/archdots/bash/.bashrc ~/.bashrc
 ln -s ~/archdots/chrome-flags.conf ~/.config/chrome-flags.conf
 ln -s ~/archdots/wallpapers ~/Pictures/wallpapers
-ln -s ~/archdots/.face ~/.face
-ln -s ~/archdots/.config/xfce4 ~/.config/xfc4
-ln -s ~/archdots/.config/polybar ~/.config/polybar
-ln -s ~/archdots/.config/mimeapps.list ~/.config/mimeapps.list
-ln -s ~/archdots/.config/picom.conf ~/.config/picom.conf
-ln -s ~/archdots/.config/sxhkd ~/.config/sxhkd
-ln -s ~/archdots/.config/bspwm ~/.config/bspwm
-ln -s ~/archdots/.config/nitrogen ~/.config/nitrogen
-ln -s ~/archdots/.config/xsettingsd ~/.config/xsettingsd
-ln -s ~/archdots/.config/Kvantum ~/.config/Kvantum
-ln -s ~/archdots/.config/qt5ct ~/.config/qt5ct
-ln -s ~/archdots/.config/dunst ~/.config/dunst
 
-
+sudo systemctl enable tlp
 sudo systemctl enable --now apparmor.service
 sudo systemctl enable --now snapd.apparmor.service
 sudo systemctl enable snapd.socket
-sudo systemctl enable bluetooth
-sudo nano /etc/bluetooth/main.conf
 
 git config --global user.email "nigelwestland@gmail.com"
 git config --global user.name "nigelwarning"
 
 flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.org/kdeapps.flatpakrepo
-flatpak --user override --filesystem=/home/$USER/.icons/:ro
+echo "export QT_STYLE_OVERRIDE=kvantum" >> ~/.profile
+
 
 
 
