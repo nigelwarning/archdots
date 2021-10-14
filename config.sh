4a9423504d26a8485bad693baf2e66e72c2d55d0
nigelwarning <nigelwestland@gmail.com>
2021-10-14 03:57:42 +0200

    bspwm back

--- config.sh
+++ config.sh
@@ -1,6 +1,3 @@
-sudo groupadd nigel
-sudo gpasswd -a nigel nigel
-
 cd
 
 sudo rm -rf .bashrc 
@@ -8,13 +5,26 @@ sudo rm -rf .bashrc
 ln -s ~/archdots/bash/.bashrc ~/.bashrc
 ln -s ~/archdots/chrome-flags.conf ~/.config/chrome-flags.conf
 ln -s ~/archdots/wallpapers ~/Pictures/wallpapers
+ln -s ~/archdots/.config/bspwm ~/.config/bspwm
+ln -s ~/archdots/.config/sxhkd ~/.config/sxhkd
+ln -s ~/archdots/.icons ~/.icons
+ln -s ~/archdots/.config/polybar ~/.config/polybar
+ln -s ~/archdots/.config/xsettingsd ~/.config/xsettingsd
+ln -s ~/archdots/.config/Kvantum ~/.config/Kvantum
+ln -s ~/archdots/.config/dconf ~/.config/dconf
+ln -s ~/archdots/.config/qt5ct ~/.config/qt5ct
+ln -s ~/archdots/.config/dunst ~/.config/dunst
+ln -s ~/archdots/.config/mimeapps.list ~/.config/mimeapps.list
+ln -s ~/archdots/.config/picom.conf ~/.config/picom.conf
 
 sudo systemctl enable tlp
 sudo systemctl enable --now apparmor.service
 sudo systemctl enable --now snapd.apparmor.service
 sudo systemctl enable snapd.socket
+sudo systemctl enable bluetooth
 eos-update-notifier -init
 eos-update-notifier -conf
+
 
 git config --global user.email "nigelwestland@gmail.com"
 git config --global user.name "nigelwarning"
--- repos.sh
+++ repos.sh
@@ -2,7 +2,10 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 
+sudo pacman-key --lsign-key 74F5DE85A506BF64
+
 sudo pacman-key --keyserver keyserver.ubuntu.com -r 003DB8B0CB23504F
 sudo pacman-key --lsign 003DB8B0CB23504F
+
 
 yay
